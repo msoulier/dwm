18c2e575392eca88ec5da76bb97a57d14f0481c9
Michael P. Soulier <msoulier@digitaltorque.ca>
2025-12-22 14:06:33 -0500

    Customized for new laptop.

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -6,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:style=Regular:size=10", "monospace:size=10", "status:style=Regular" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMonoNLNerdFontMono:style=Regular:size=14", "monospace:size=14", "status:style=Regular" };
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -81,10 +82,19 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "/usr/bin/x-terminal-emulator", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
-static const char *volcmd[]  = { "pavucontrol", NULL };
-static const char *rangercmd[]  = { "/usr/bin/x-terminal-emulator", "-e", "ranger", NULL };
+//static const char *volcmd[]  = { "alacritty", "-e", "pamix", NULL };
+static const char *filemgr[]  = { "thunar", NULL };
+static const char *homecal[] = { "chromium", "--app=https://calendar.google.com", NULL };
+static const char *workcal[] = { "chromium", "--app=https://outlook.office365.com/calendar/view/week", NULL };
+static const char *slackcmd[] = { "chromium", "--app=https://app.slack.com/client/T7CAAK9NJ/C7BHR0GMB", NULL };
+static const char *spotifycmd[] = { "chromium", "--app=https://open.spotify.com", NULL };
+static const char *whatsappcmd[] = { "chromium", "--app=https://web.whatsapp.com", NULL };
+static const char *volcmd[] = { "pavucontrol", NULL };
+static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+static const char *volmuttogglecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
@@ -92,7 +102,15 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
     { MODKEY|ControlMask,           XK_l,      spawn,          {.v = lockcmd } },
     { MODKEY|ControlMask,           XK_v,      spawn,          {.v = volcmd } },
-    { MODKEY|ControlMask,           XK_e,      spawn,          {.v = rangercmd } },
+    { MODKEY|ControlMask,           XK_e,      spawn,          {.v = filemgr } },
+    { MODKEY|ControlMask,           XK_c,      spawn,          {.v = homecal } },
+    { MODKEY|ControlMask|ShiftMask, XK_c,      spawn,          {.v = workcal } },
+    { MODKEY|ControlMask,           XK_s,      spawn,          {.v = slackcmd } },
+    { MODKEY|ControlMask|ShiftMask, XK_s,      spawn,          {.v = spotifycmd } },
+    { MODKEY|ControlMask,           XK_w,      spawn,          {.v = whatsappcmd } },
+    { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
+    { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
+    { 0,                            XF86XK_AudioMute, spawn, {.v = volmuttogglecmd } },
     { MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
