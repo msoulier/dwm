e1826680a7c6c9b8210eff098e493f04b637258e
Michael P. Soulier <msoulier@digitaltorque.ca>
2026-03-20 16:42:57 -0400

    Consolidated affogato and ramirez branches

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -6,8 +7,9 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:style=Regular:size=10", "monospace:size=10", "status:style=Regular" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMonoNLNerdFontMono:style=Regular:size=16", "monospace:size=16", "status:style=Regular" };
+//static const char *fonts[]          = { "JetBrainsMonoNLNerdFontMono:style=Regular:size=12", "monospace:size=12", "status:style=Regular" };
+static const char dmenufont[]       = "monospace:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -44,10 +46,9 @@ static const Rule rules[] = {
     /* class                    instance    title       tags mask     isfloating   monitor */
     { "Gimp",                   NULL,       NULL,       0,            1,           -1 },
     { "pavucontrol",            NULL,       NULL,       0,            1,           -1 },
-    { "qjackctl",               NULL,       NULL,       0,            1,           -1 },
-    { "rakarrack",              NULL,       NULL,       0,            1,           -1 },
     { "qemu-system-x86_64",     NULL,       NULL,       0,            1,           -1 },
     { "audacity",               NULL,       NULL,       0,            1,           -1 },
+    { "davmail-DavGateway",     NULL,       NULL,       0,            1,           -1 },
     //{ "Firefox",    NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -69,6 +70,7 @@ static const Layout layouts[] = {
 /* key definitions */
 // Use the windows key as the mod key. Use Mod1Mask to set it to Alt.
 #define MODKEY Mod4Mask
+#define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
     { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -80,19 +82,46 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *networkcmd[] = { "nm-connection-editor", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "/usr/bin/x-terminal-emulator", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
-static const char *volcmd[]  = { "pavucontrol", NULL };
-static const char *rangercmd[]  = { "/usr/bin/x-terminal-emulator", "-e", "ranger", NULL };
+static const char *tfilemgr[]  = { "alacritty", "-e", "ranger", NULL };
+static const char *gfilemgr[]  = { "thunar", NULL };
+//static const char *volcmd[]  = { "alacritty", "-e", "pamix", NULL };
+static const char *homecal[] = { "brave-browser", "--app=https://calendar.google.com", NULL };
+static const char *workcal[] = { "brave-browser", "--app=https://outlook.office365.com/calendar/view/week", NULL };
+static const char *slackcmd[] = { "brave-browser", "--app=https://app.slack.com/client/T7CAAK9NJ/C7BHR0GMB", NULL };
+static const char *spotifycmd[] = { "chromium", "--app=https://open.spotify.com", NULL };
+static const char *whatsappcmd[] = { "brave-browser", "--app=https://web.whatsapp.com", NULL };
+static const char *heycafecmd[] = { "brave-browser", "--app=https://hey.cafe", NULL };
+static const char *feedlycmd[] = { "brave-browser", "--app=https://feedly.com/i/my/me", NULL };
+static const char *teamscmd[] = { "brave-browser", "--app=https://teams.microsoft.com/v2/", NULL };
+static const char *volcmd[] = { "pavucontrol", NULL };
+static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+static const char *volmuttogglecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
+    { ALTKEY|ControlMask,           XK_n,      spawn,          {.v = networkcmd } },
     { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
     { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY|ControlMask,           XK_l,      spawn,          {.v = lockcmd } },
-    { MODKEY|ControlMask,           XK_v,      spawn,          {.v = volcmd } },
-    { MODKEY|ControlMask,           XK_e,      spawn,          {.v = rangercmd } },
+    { ALTKEY|ControlMask,           XK_l,      spawn,          {.v = lockcmd } },
+    { ALTKEY|ControlMask,           XK_v,      spawn,          {.v = volcmd } },
+    { ALTKEY|ControlMask,           XK_e,      spawn,          {.v = tfilemgr } },
+    { ALTKEY|ControlMask|ShiftMask, XK_e,      spawn,          {.v = gfilemgr } },
+    { ALTKEY|ControlMask,           XK_c,      spawn,          {.v = homecal } },
+    { ALTKEY|ControlMask|ShiftMask, XK_c,      spawn,          {.v = workcal } },
+    { ALTKEY|ControlMask,           XK_s,      spawn,          {.v = slackcmd } },
+    { ALTKEY|ControlMask|ShiftMask, XK_s,      spawn,          {.v = spotifycmd } },
+    { ALTKEY|ControlMask,           XK_w,      spawn,          {.v = whatsappcmd } },
+    { ALTKEY|ControlMask,           XK_h,      spawn,          {.v = heycafecmd } },
+    { ALTKEY|ControlMask,           XK_f,      spawn,          {.v = feedlycmd } },
+    { ALTKEY|ControlMask|ShiftMask, XK_t,      spawn,          {.v = teamscmd } },
+    { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
+    { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
+    { 0,                            XF86XK_AudioMute, spawn, {.v = volmuttogglecmd } },
     { MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -143,7 +172,7 @@ static Button buttons[] = {
     { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
     { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
     { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+    { ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
     { ClkTagBar,            0,              Button1,        view,           {0} },
     { ClkTagBar,            0,              Button3,        toggleview,     {0} },
     { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
